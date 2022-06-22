75d2153d6a58733289924584810cd6c4dbf87ae2
Tassnem zidan <tasneemzidan7@gmail.com>
2022-06-22 10:10:19 +0200

    Add css

--- crud-react-system/src/App.jsx
+++ crud-react-system/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { db } from './firebase-config';
+
 import {
   addDoc,
   collection,
@@ -43,28 +44,45 @@ function App() {
 
   return (
     <div className="App">
-      <input placeholder="Name..."
-        onChange={(event) => {
-          setNewName(event.target.value);
-        }} />
-      <input type="number" placeholder="Age..."
-        onChange={(event) => {
-          setNewAge(event.target.value);
-        }}
-      />
+      <table>
+        <input placeholder="Name..."
+          onChange={(event) => {
+            setNewName(event.target.value);
+          }} />
+        <input type="number" placeholder="Age..."
+          onChange={(event) => {
+            setNewAge(event.target.value);
+          }}
+        />
 
-      <button onClick={createUser}> Create User</button>
-      {users.map((user) => {
-        return (
-          <div>
-            {" "}
-            <h1>Name: {user.name}</h1>
-            <h1>Age: {user.age}</h1>
-            <button onClick={() => { updateUser(user.id, user.age) }}>Increase age</button>
-            <button onClick={() => { delteUser(user.id, user.age) }}>Delete User</button>
-          </div>
-        );
-      })}
+        <button onClick={createUser}> Create User</button>
+        {users.map((user) => {
+          return (
+            <div>
+              {" "}
+
+              <tr>
+                <th>Name: </th>
+                <th>Age: </th>
+              </tr>
+
+              <tr >
+                <td>{user.name}</td>
+
+                <td>{user.age}</td>
+              </tr>
+
+
+
+
+              <button onClick={() => { updateUser(user.id, user.age) }}>Increase age</button>
+              <button onClick={() => { delteUser(user.id, user.age) }}>Delete User</button>
+            </div>
+
+
+          );
+        })}
+      </table>
     </div>
   );
 }
